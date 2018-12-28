7efa8b6d19a8b455478e2bd2f1b7b9c924f1d89e
Rahul Roy <rahulroy.soe@gmail.com>
2018-12-28 19:43:32 +0530

    Python_Selenium_Code

---
+++ tests/login_test.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from pages.login_page import LoginPage
+from pages.home_page import HomePage
+from pages.admin_page import Admin
+from utils.constants import *
+import pytest
+#import allure
+
+@pytest.mark.usefixtures("test_setup")
+class TestLogin():
+
+    def test_login(self):
+        driver = self.driver
+        driver.get(URL)
+        login = LoginPage(driver)
+        login.test_login(USERNAME, PASSWORD)
+
+    def test_homepage_basic(self):
+        driver = self.driver
+        global home
+        home = HomePage(driver)
+        home.test_navigation("Admin")
+        home.test_navigation("PIM")
+        home.test_navigation("Leave")
+        home.test_navigation("Time")
+        home.test_navigation("Performance")
+        home.test_navigation("Dashboard")
+        home.test_navigation("Directory")
+
+    def test_search_a_user(self):
+        driver = self.driver
+        page = Admin(driver)
+        page.search_user()
+
+    def test_logout(self):
+        # home = HomePage(driver)
+        home.test_logout()
---
+++ utils/constants.py
@@ -0,0 +1,27 @@
+import moment
+#import allure
+import json
+
+URL = "https://opensource-demo.orangehrmlive.com/"
+USERNAME = "Admin"
+PASSWORD = "admin123"
+
+
+def screenshot(self):
+    print("Inside Screenshot Def")
+    x = moment.now().strftime("%d-%m-%Y_%H-%M-%S")
+    y = "screenshot_" + x
+    print("Captured the Screenshot named::", y)
+    self.driver.get_screenshot_as_file("C:/Users/Lenovo/Downloads/SeleniumPythonPOMFramework-master/data/" +y+ ".PNG")
+
+    #Below line works fine with allure adopter
+    # allure.MASTER_HELPER.attach(name=y, contents=self.driver.get_screenshot_as_png(),
+    #                             type=allure.MASTER_HELPER.attach_type.PNG)
+
+    #Below line works fine with allure-pytest
+ #   allure.attach(self.driver.get_screenshot_as_png(),name=y,attachment_type=allure.attachment_type.PNG)
+
+def open_json_file(filepath):
+    with open(filepath) as jsonFile:
+        jsonData = json.load(jsonFile)
+        return jsonData
